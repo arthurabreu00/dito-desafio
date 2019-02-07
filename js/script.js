257a0cb5ce25ee12f5d1e239a203d1f76ea1dcc7
Arthur Tavares <arthurabreu00@gmail.com>
2019-02-07 21:26:43 -0200

    Finalizando parte visual

--- js/script.js
+++ js/script.js
@@ -1,32 +1,30 @@
 // Depêdemcia do tooltipe (Caixa de dúvida / Ponto de interrogação).
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-});
-
-(function initEvents(){
-
-    // Ao colocar o mouse
-    document.querySelector('.table').addEventListener('mouseover',e =>{
-        let nameStoreEl = e.target.parentNode.querySelector('.name-store');
-        // let tdNameStorageEl = e.target.parentNode.parentNode.querySelector('.name-store');
-
-
-        // Caso exista o elemento procurado.
-        if(nameStoreEl || tdNameStorageEl){
-            nameStoreEl.style.color = '#A6E0BE';
-        };
-    });
-
-    // Ao retirar o mouse 
-    document.querySelector('.table').addEventListener('mouseout',e =>{
-
-        let nameStoreEl = e.target.parentNode.querySelector('.name-store');
-        let tdNameStorageEl = e.target.parentNode.parentNode.querySelector('.name-store');
-
-
-        if(nameStoreEl || tdNameStorageEl ){
-            nameStoreEl.style.color = '#000';
-        }
-     });
+    $('[data-toggle="tooltip"]').tooltip();
     
+});
+(function(){
+    $.ajax({
+        type: 'GET',
+        url: 'https://storage.googleapis.com/dito-questions/survey-responses.json',
+        success: function(data) {
+            listAll(JSON.parse(data));
+      scrollTo }
+     });    
 })();
+
+
+// (function(){
+//     var ajax = new XMLHttpRequest();
+//     ajax.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             listAll(this.response);
+//         }
+//     };
+//     ajax.open("GET",'https://storage.googleapis.com/dito-questions/survey-responses.json', true);
+//     ajax.send();
+// })();
+
+function listAll(res){
+    console.log(res);
+}
