e0e7b6d6eab7176f8d16be01ee096bea0d99d2ef
Arthur Tavares <arthurabreu00@gmail.com>
2019-02-07 12:26:31 -0200

    Table

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+// Depêdemcia do tooltipe (Caixa de dúvida / Ponto de interrogação).
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 });
+
+(function initEvents(){
+
+    // Ao colocar o mouse
+    document.querySelector('.table').addEventListener('mouseover',e =>{
+        let nameStoreEl = e.target.parentNode.querySelector('.name-store');
+        // let tdNameStorageEl = e.target.parentNode.parentNode.querySelector('.name-store');
+
+
+        // Caso exista o elemento procurado.
+        if(nameStoreEl || tdNameStorageEl){
+            nameStoreEl.style.color = '#A6E0BE';
+        };
+    });
+
+    // Ao retirar o mouse 
+    document.querySelector('.table').addEventListener('mouseout',e =>{
+
+        let nameStoreEl = e.target.parentNode.querySelector('.name-store');
+        let tdNameStorageEl = e.target.parentNode.parentNode.querySelector('.name-store');
+
+
+        if(nameStoreEl || tdNameStorageEl ){
+            nameStoreEl.style.color = '#000';
+        }
+     });
+    
+})();
